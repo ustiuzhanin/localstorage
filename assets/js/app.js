9596e46327678c02ae1411212dd1a06a4fc1d437
Slava <ucheba00@gmail.com>
2019-01-07 02:59:32 -0700

    add and remove tweets to localstorage

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,35 +5,102 @@ const tweetList = document.querySelector('#tweet-list');
 const tweetInput = document.querySelector('#tweet');
 const form = document.querySelector('#form');
 
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    if (tweetInput.value !== '') {
+        createNewTweet();
+    }
+    this.reset();
+};
+
+//add tweet
+
 function createNewTweet() {
     const tweetElement = document.createElement('li');
     const closeTweetElement = document.createElement('a');
 
-    //add tweet
-
     closeTweetElement.classList.add('remove-tweet');
     closeTweetElement.innerText = 'X';
 
-    tweetElement.innerText = tweetInput.value;
+    tweetElement.textContent = tweetInput.value;
 
     tweetElement.appendChild(closeTweetElement);
     tweetList.appendChild(tweetElement);
 
-    //remove tweet
+    // add tweet to local storage
 
-    function onCloseBtnClick(evt) {
-        tweetList.removeChild(evt.target.parentElement);
+    addTweetToLocalStorage(tweetInput.value);
+};
 
+function addTweetToLocalStorage(tweet) {
+    let tweetsFromLS = getTweetsFromLocalStorage();
+
+    tweetsFromLS.push(tweetInput.value);
+
+    localStorage.setItem('tweets', JSON.stringify(tweetsFromLS));
+};
+
+function getTweetsFromLocalStorage() {
+    let tweets;
+    const tweetsLS = localStorage.getItem('tweets');
+
+    if (tweetsLS === null) {
+        tweets = [];
+    } else {
+        tweets = JSON.parse(tweetsLS);
     }
 
-    closeTweetElement.addEventListener('click', onCloseBtnClick);
-}
+    return tweets;
+};
 
+// remove tweet
 
-function onFormSubmit() {
-    if (tweetInput.value !== '') {
-        createNewTweet();
+function onCloseBtnClick(evt) {
+    const targetParent = evt.target.parentElement;
+    if (evt.target.classList.contains('remove-tweet')) {
+        tweetList.removeChild(targetParent);
+        removeTweetFromLocalStorage(targetParent.textContent);
     }
+};
+
+// remove tweet form local storage
+
+function removeTweetFromLocalStorage (tweet) {
+    let tweetsLS = getTweetsFromLocalStorage();
+
+    const deleteTweet = tweet.slice(0, -1);
+
+    tweetsLS.some(function(item, index) {
+        if (deleteTweet === item) {
+            tweetsLS.splice(index, 1);
+            return true;
+        }
+      });
+
+    localStorage.setItem('tweets', JSON.stringify(tweetsLS));
 }
+
+// load tweets from local storage
+
+function onPageLoadStorageCheck() {
+    let tweetsLS = getTweetsFromLocalStorage();
+
+    tweetsLS.forEach(function (item) {
+        const tweetElement = document.createElement('li');
+        const closeTweetElement = document.createElement('a');
+
+        closeTweetElement.classList.add('remove-tweet');
+        closeTweetElement.innerText = 'X';
+
+        tweetElement.innerText = item;
+
+        tweetElement.appendChild(closeTweetElement);
+        tweetList.appendChild(tweetElement);
+    });
+};
+
+// event listeners
 
 form.addEventListener('submit', onFormSubmit);
+tweetList.addEventListener('click', onCloseBtnClick);
+document.addEventListener('DOMContentLoaded', onPageLoadStorageCheck);
